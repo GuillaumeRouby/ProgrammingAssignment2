1038c2527f1730ada63cd77f64943695d278c867
guillaumerby <guillaumerby@gmail.com>
2015-02-22 14:11:46 -0500

    GuillaumeRouby
    Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Put comments here that give an overall description of what your functions do
+# Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## makeCacheMatrix is a function that creates a special "matrix" object that can cache its inverse
+## makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Creates a list of functions that
+  ## can cache the inverse of a matrix.
+  matrixinv <- NULL
+  set <- function(y) {
+    x <<- y
+    matrixinv <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) matrixinv <<- inverse
+  getInverse <- function() matrixinv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
 ## Write a short comment describing this function
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  matrixinv <- x$getinverse()
+  if(!is.null(matrixinv)) {
+    message("getting cached data")
+    return(matrixinv)
+  }
+  data <- x$get()
+  matrixinv <- solve(data)
+  x$setinverse(matrixinv)
+  matrixinv
 }
